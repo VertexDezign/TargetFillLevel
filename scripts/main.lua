68180f4b88ec3f66f6cc943071c48440be71ec71
André Buchmann <andy.schliesser@gmail.com>
2021-12-31 00:22:19 +0100

    Prepare mod for release

--- scripts/main.lua
+++ scripts/main.lua
@@ -12,11 +12,7 @@ Feel free to open a pull reuests for enhancements or bugfixes.
 You are not allowed to sell this or a modified version of the mod.
 ]] ----------------------------------------------------------------------------------------------------
 
--- Load inspect for debugging
--- local modDir = g_currentModDirectory
--- source(modDir .. "scripts/lib/inspect.lua");
 TargetFillLevel = {}
-local TargetFillLevel_mt = Class(TargetFillLevel)
 
 function TargetFillLevel:getFillLevelInformation(superFunc, display)
     superFunc(self, display)
@@ -47,20 +43,31 @@ function TargetFillLevel:getFillLevelInformation(superFunc, display)
             return
         end
 
-        print('TFL: Valid trailer in trigger')
-        if trailer.getFillLevelInformation ~= nil then
-            trailer:getFillLevelInformation(display)
-        elseif trailer.getFillLevel ~= nil and trailer.getFillType ~= nil then
-            local fillType = trailer:getFillType()
-            local fillLevel = trailer:getFillLevel()
-            local capacity = fillLevel
-
-            if trailer.getCapacity ~= nil then
-                capacity = trailer:getCapacity()
-            end
-
-            display:addFillLevel(fillType, fillLevel, capacity)
+        --[[ The fill level could be added to any combine, but as they are concatenated
+        by type, it doesn't make sence to display them. Therefor it will be displayed
+        only for forage harvesters.
+        ]]
+        -- detect forage harvester as AI does it
+        local dischargeNode = combine:getCurrentDischargeNode()
+        if dischargeNode ~= nil and combine:getFillUnitCapacity(dischargeNode.fillUnitIndex) == math.huge then
+            TargetFillLevel:addFillLevelDisplay(trailer, display)
         end
+    end
+end
+
+function TargetFillLevel:addFillLevelDisplay(targetVehicle, display)
+    if targetVehicle.getFillLevelInformation ~= nil then
+        targetVehicle:getFillLevelInformation(display)
+    elseif targetVehicle.getFillLevel ~= nil and targetVehicle.getFillType ~= nil then
+        local fillType = targetVehicle:getFillType()
+        local fillLevel = targetVehicle:getFillLevel()
+        local capacity = fillLevel
+
+        if targetVehicle.getCapacity ~= nil then
+            capacity = targetVehicle:getCapacity()
+        end
+
+        display:addFillLevel(fillType, fillLevel, capacity)
     end
 end
 
@@ -73,10 +80,7 @@ function initTargetFillLevel(name)
     addModEventListener(TargetFillLevel)
 
     -- Hook onto fill level display
-    Vehicle.getFillLevelInformation = Utils.overwrittenFunction(
-        Vehicle.getFillLevelInformation,
-        TargetFillLevel.getFillLevelInformation
-    )
+    Vehicle.getFillLevelInformation = Utils.overwrittenFunction(Vehicle.getFillLevelInformation, TargetFillLevel.getFillLevelInformation)
 end
 
 initTargetFillLevel(g_currentModName)
